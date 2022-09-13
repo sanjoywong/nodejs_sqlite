6682c4ec42675a61e9d64055e5cd5ad0641d7ea3
sanjoy.wong <sanjoy.wong@gmail.com>
2022-09-13 17:01:38 +0200

    utiliser db.get et db.each

--- index.js
+++ index.js
@@ -7,22 +7,34 @@ let db = new sqlite3.Database(dbname,(err)=>{
     console.log("Base de nonnée initialisé dans le fichier :" + dbname);
 });
 
-//db.run('CREATE TABLE IF NOT EXISTS planetes(name varchar(25));');
+//db.run("drop table planetes");
 
-/* db.run("insert into planetes(name) values(?)",["Mars"]);
-db.run("insert into planetes(name) values(?)",["Terre"]);
-db.run("insert into planetes(name) values(?)",["Pluton"]);
-db.run("insert into planetes(name) values(?)",["Jupiter"]);
-db.run("insert into planetes(name) values(?)",["Mercure"]);
- */
+ //db.run('CREATE TABLE IF NOT EXISTS planetes(id INTEGER PRIMARY KEY AUTOINCREMENT,name varchar(25),size varchar(255),created_at datetime default current_timestamp not null);');
+
+/* db.run("insert into planetes(name,size) values(?,?)",["Mars","3454645"]);
+db.run("insert into planetes(name,size) values(?,?)",["Pluton","64654654"]);
+db.run("insert into planetes(name,size) values(?,?)",["Jupiter","746464"]);
+db.run("insert into planetes(name,size) values(?,?)",["Terre","564656565"]);
+db.run("insert into planetes(name,size) values(?,?)",["Mercure","876464654"]);  */
+
  
 //db.run("create unique index rowid on planetes(name)");
 //db.run("drop index rowid ");
 //db.run("alter table planetes drop rowid ");
 
-db.all("select rowid,* from planetes",(err,data)=>{
+/* db.all("select * from planetes",(err,data)=>{
     if(err) throw err;
     console.log(data);
 }); 
+ */
 
+/* db.each("select * from planetes",(err,data)=>{
+    if(err) throw err;
+    console.log(data.id,data.name,data.size);
+}); */
+console.log("la premiere ligne :")
+db.get("select * from planetes",(err,data)=>{
+    if(err) throw err;
+    console.log(data.id,data.name,data.size);
+});
 //db.run("delete from planetes");
